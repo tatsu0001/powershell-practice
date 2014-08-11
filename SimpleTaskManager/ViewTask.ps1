5a6a43aa406a3cf92bc331e6e01276792ba96f09
Tatsunosuke Miyagi <d.junkyard@gmail.com>
2014-08-12 01:27:57 +0900

    output csv

--- SimpleTaskManager/ViewTask.ps1
+++ SimpleTaskManager/ViewTask.ps1
@@ -1,26 +1,40 @@
 
-param([Switch] $Verbose,
-    [String] $TaskName = $null)
+param([String] $TaskName = $null,
+      [Switch] $GridView,
+      [String] $Csv = $null)
 
 $ErrorActionPreference = "Stop"
 
 function Get-TaskList(
-    [Boolean] $Verbose,
     [String] $TaskName = $null) {
 
-    $Query="schtasks.exe /Query /FO CSV "
-    if ($Verbose) {
-        $Query += " /V"
-    }
-    [Object[]] $taskList = (Invoke-Expression "& $Query" | ConvertFrom-Csv | ? { -not ($_.タスク名 -eq "タスク名") } )
+    $query="schtasks.exe /Query /FO CSV /V "
+    [Object[]] $taskList = (Invoke-Expression "& $query" | ConvertFrom-Csv | ? { -not ($_.タスク名 -eq "タスク名") } )
     if ($TaskName) {
         $taskList = $taskList | ? { $_.タスク名 -cmatch [Regex]$TaskName }
     }
 
-    $taskList
+    return $taskList
 }
 
-# タスク一覧表示
-$taskList = Get-TaskList -Verbose $Verbose -TaskName $TaskName
-$taskList | Out-GridView -Title "スケジュールタスク一覧" | Out-Null
+# タスク一覧
+$taskList = Get-TaskList -TaskName $TaskName
+
+# ファイル出力
+if ($Csv) {
+    if (Test-Path -Path $Csv -PathType Leaf) {
+        Write-Host "ERROR: $Csv が既に存在します。"
+    }
+
+    $taskList | Export-Csv -Path $Csv -Encoding OEM -NoTypeInformation > $null
+}
+
+# グリッド表示
+if ($GridView) {
+    $taskList | Out-GridView -Title "スケジュールタスク一覧" 
+    # GridViewが閉じるのを待つ
+    while([System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle -ne 0){
+        start-sleep 2
+    }
+}
 
